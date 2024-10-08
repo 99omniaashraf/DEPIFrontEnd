ea611a21047b9d07151263cddce3dfc120e3f4fa
99omniaashraf <omniaashraf249@gmail.com>
2024-10-08 23:29:40 +0300

    component

--- ecommerce-website/src/app/app.module.ts
+++ ecommerce-website/src/app/app.module.ts
@@ -1,36 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductListingComponent } from './components/product-listing/product-listing.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'products', component: ProductListingComponent },
-  { path: 'products/:id', component: ProductDetailsComponent },
-  { path: 'cart', component: ShoppingCartComponent },
-  { path: 'checkout', component: CheckoutComponent },
   { path: 'about-us', component: AboutUsComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ProductListingComponent,
-    ProductDetailsComponent,
-    ShoppingCartComponent,
-    CheckoutComponent,
     AboutUsComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
