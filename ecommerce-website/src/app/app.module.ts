8bafcc6320c95d136ece14efb8f0c0a9f1cd0664
shehab-dev1 <shehab2249@gmail.com>
2024-10-08 23:08:26 +0300

    update

--- ecommerce-website/src/app/app.module.ts
+++ ecommerce-website/src/app/app.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductListingComponent } from './components/product-listing/product-listing.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
@@ -23,14 +18,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ProductListingComponent,
-    ProductDetailsComponent,
     ShoppingCartComponent,
     CheckoutComponent,
     AboutUsComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
