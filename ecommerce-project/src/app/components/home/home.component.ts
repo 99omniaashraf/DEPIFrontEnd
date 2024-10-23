7d7e7d1ad53d4f3d89aaff396a80ccfbd88d9266
99omniaashraf <omniaashraf249@gmail.com>
2024-10-23 03:13:45 +0300

    project

--- ecommerce-project/src/app/components/home/home.component.ts
+++ ecommerce-project/src/app/components/home/home.component.ts
@@ -22,19 +22,19 @@ export class HomeComponent {
       name: 'Product 1',
       description: 'Description for Product 1',
       price: 99.99,
-      imageUrl: 'assets/product1.jpg'
+      imageUrl: '/assets/images/picture1.jpg'
     },
     {
       name: 'Product 2',
       description: 'Description for Product 2',
       price: 149.99,
-      imageUrl: 'assets/product2.jpg'
+      imageUrl: '/assets/images/picture2.jpg'
     },
     {
       name: 'Product 3',
       description: 'Description for Product 3',
       price: 199.99,
-      imageUrl: 'assets/product3.jpg'
+      imageUrl: '/assets/images/picture6.jpg'
     }
   ];
 
--- ecommerce-project/src/app/components/product-listing/product-listing.component.ts
+++ ecommerce-project/src/app/components/product-listing/product-listing.component.ts
@@ -16,21 +16,21 @@ export class ProductListingComponent implements OnInit {
       name: 'Product 1',
       description: 'Description for Product 1',
       price: 100,
-      imageUrl: '/ecommerce-project/src/assets/img/picture9.jpg'
+      imageUrl: '/assets/images/picture1.jpg'
     },
     {
       id: 2,
       name: 'Product 2',
       description: 'Description for Product 2',
       price: 200,
-      imageUrl: '/ecommerce-project/src/assets/img/picture13.jpg'
+      imageUrl: '/assets/images/picture2.jpg'
     },
     {
       id: 3,
       name: 'Product 3',
       description: 'Description for Product 3',
       price: 300,
-      imageUrl: '/ecommerce-project/src/assets/img/picture12.jpg'
+      imageUrl: '/assets/images/picture6.jpg'
     }
   ];
 
